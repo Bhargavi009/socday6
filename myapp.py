33aa5aa2312f2091615d5a3fe1ba0deb61c3304f
Bhargavi009 <134853385+Bhargavi009@users.noreply.github.com>
2024-01-08 12:13:06 +0530

    myapp.py

--- myapp.py
+++ myapp.py
@@ -5,9 +5,9 @@ st.header("Types of Cats")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("Persian Cat")
-  st.image("https://www.google.com/search?q=videos+link+on+ml&rlz=1C1GCEA_enIN1042IN1042&oq=videos+link+on+ml&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIJCAEQIRgKGKABMgkIAhAhGAoYoAEyCggDECEYFhgdGB7SAQkxMDMwMmoxajeoAgCwAgA&sourceid=chrome&ie=UTF-8#",caption="Persian Cat",width=300,use_column_width=True)
+  st.image("https://github.com/Bhargavi009/socday6/blob/main/persian.jpg",caption="Persian Cat",width=300,use_column_width=True)
   st.write("Persian cats are cute")
   with col2:
     st.subheader("Ragdoll Cat")
-  st.image("https://github.com/Bhargavi009/socday6/blob/main/ragroll.jpg",caption="Ragdoll Cat",width=300,use_column_width=True)
+  st.image("https://github.com/Bhargavi009/socday6/blob/main/ragroll.jpg",caption="Ragdoll Cat",width=3000,use_column_width=True)
   st.write("Ragdoll cats are proud")
