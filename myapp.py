924a69493823ed89825857ad72d6656fec17f138
Bhargavi009 <134853385+Bhargavi009@users.noreply.github.com>
2024-01-08 12:18:34 +0530

    myapp.py

--- myapp.py
+++ myapp.py
@@ -5,7 +5,7 @@ st.header("Types of Cats")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("Persian Cat")
-  st.video("https://www.youtube.com/watch?v=_Z9TRANg4c0",caption="Persian Cat",width=300,use_column_width=True)
+  st.video("https://www.youtube.com/watch?v=_Z9TRANg4c0",format="video/mp4", start_time=0)
   st.write("Persian cats are cute")
   with col2:
     st.subheader("Ragdoll Cat")
