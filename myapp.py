82cf244dea14b7a3ffc3cd1ca686747dba0308ce
Bhargavi009 <134853385+Bhargavi009@users.noreply.github.com>
2024-01-08 12:10:15 +0530

    myapp.py

--- myapp.py
+++ myapp.py
@@ -5,7 +5,7 @@ st.header("Types of Cats")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("Persian Cat")
-  st.image("https://www.youtube.com/watch?v=6Bk0oH_ArWc",caption="Persian Cat",width=300,use_column_width=True)
+  st.image("https://www.google.com/search?q=videos+link+on+ml&rlz=1C1GCEA_enIN1042IN1042&oq=videos+link+on+ml&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIJCAEQIRgKGKABMgkIAhAhGAoYoAEyCggDECEYFhgdGB7SAQkxMDMwMmoxajeoAgCwAgA&sourceid=chrome&ie=UTF-8#",caption="Persian Cat",width=300,use_column_width=True)
   st.write("Persian cats are cute")
   with col2:
     st.subheader("Ragdoll Cat")
